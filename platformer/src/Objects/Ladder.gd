e6a3e4119c540deea8e85096ae6399a8068fb4bc
Raestkjoot <gunnarmagnum@gmail.com>
2023-09-25 20:41:16 +0200

    very crudely implemented ladder

--- platformer/src/Objects/Ladder.gd
+++ platformer/src/Objects/Ladder.gd
@@ -1,30 +1,52 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var player
-var is_player_climbing = false
+var is_player_close = false
+var enter_climbing = false
+var climb = false
+var exit_climbing = false
+var original_pos
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# I know this is insanely ugly, but I don't care B)
 func _process(delta):
-	if is_player_climbing == true:
-		player.global_translate(Vector2(0.2, -3.0))
+	if is_player_close == true:
+		$Label.show()
+		if Input.is_action_just_pressed("jump"):
+			enter_climbing = true
+			player.is_climbing = true
+	if exit_climbing == true:
+		player.global_position.x += delta * 50.0
+		if player.global_position.x > original_pos + 30.0:
+			exit_climbing = false
+			enter_climbing = false
+			climb = false
+			is_player_close = false
+			player.is_climbing = false
+			player._velocity.y = 0.0
+			player.fall_speed = 0.0
+	elif enter_climbing == true:
+		if climb == true:
+			player.global_translate(Vector2(0.0, -1.0))
+			if player.global_position.y < 310.0:
+				exit_climbing = true
+				original_pos = player.global_position.x
+		else:
+			player.global_position.x = player.global_position.move_toward(self.global_position, delta * 100.0).x
+			if abs(player.global_position.x - self.global_position.x) < 1.2:
+				climb = true
 
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
 		player = body
-		player.is_climbing = true
-		is_player_climbing = true
-		print("time to climb boi!")
+		is_player_close = true
 
 
 func _on_Area2D_body_exited(body):
-	pass # Replace with function body.
+	is_player_close = false
+	$Label.hide()
